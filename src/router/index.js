85ea6b6f64d87bc2d4150fdb09f098de71b03a61
THqqqqp <2472412112@qq.com>
2024-06-09 15:46:36 +0800

    引入router@4

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+
+const routes = [
+    {
+        path: '/',
+        component: () => import('../views/index.vue')
+    },
+    {
+        path: '/content',
+        component: () => import('../views/content/index.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
---
+++ src/views/content/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>内容1</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
