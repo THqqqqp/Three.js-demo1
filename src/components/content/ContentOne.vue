30ef6974e7074b15dc7995ae626023dc15cefe67
THqqqqp <2472412112@qq.com>
2024-06-09 17:26:23 +0800

    第一个建模场景

--- src/components/content/ContentOne.vue
+++ src/components/content/ContentOne.vue
@@ -1,15 +1,48 @@
 <script setup>
+import * as THREE from 'three';
+import {onMounted, ref} from "vue";
 
+const containerRef = ref();
+// 创建场景
+const scene = new THREE.Scene();
+// 创建相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(-10, 40, 30)
+camera.lookAt(scene.position)
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(new THREE.Color(0xeeeeee));
+// 创建坐标轴
+const axes = new THREE.AxesHelper(40);
+scene.add(axes);
+
+
+onMounted(() => {
+  // 设置渲染器的大小
+  renderer.setSize(containerRef.value?.clientWidth, containerRef?.value.clientHeight);
+  // 渲染
+  containerRef.value?.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera)
+})
 </script>
 
 <template>
   <div id="root">
-    内容1组件
+    <h2>一、创建一个基本的three坐标系</h2>
+    <div ref="containerRef" class="container"></div>
   </div>
 </template>
 
-<style scoped lang="scss">
-#root{
-  border: 1px solid red;
+<style lang="scss" scoped>
+#root {
+  padding: 10px;
+  height: 500px;
+
+  .container {
+    width: 100%;
+    height: 100%;
+    border: 1px solid blue;
+  }
 }
 </style>
